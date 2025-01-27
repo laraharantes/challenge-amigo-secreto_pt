fb11f46ecb0d183ad6d14c8c9dd446682be479c2
Larah Arantes <larahpraticamente16@gmail.com>
2025-01-27 17:09:37 -0300

    projeto inicial

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = [];
+let listaDeAmigos = [];
+
+//Criar função para adicionar amigos no campo de texto
+
+function adicionarAmigo(){
+    let nome = document.getElementById("amigo").value.trim();
+    if(nome === ""){
+        alert("Por favor,digite o nome de um amigo!");
+     } else {
+        listaDeAmigos.push(nome);
+        document.getElementById("amigo").value = "";
+        atualizarLista();
+        console.log(listaDeAmigos);
+    }
+}
+
+
+//Implementar uma função para atualizar a lista de amigos
+
+function atualizarLista() {
+    const listaElement = document.getElementById("listaAmigos");
+    listaElement.innerHTML = "";
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+        listaElement.appendChild(li);
+    };
+}
+
+//Implementar uma função para sortear um amigo aleaoriamente
+
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        alert("Não há amigos para sortear.");
+        return;
+    }
+
+    const indiceResultado = Math.floor(Math.random() * listaDeAmigos.length);
+    const resultado = listaDeAmigos[indiceSorteado];
+    document.getElementById("Resultado").innerHTML = "Resultado: " + amigoSorteado;
+}
